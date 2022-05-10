5a01cca10ff8981ad3d6540d317af659811075bd
Phalat Wongsitthipornrung <phalat18@gmail.com>
2022-05-10 18:16:50 +0700

    dockerfile

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const courseRoute = require('./routes/course');
+const courseRoute = require('../routes/course');
 var cors = require('cors');
 
 dotenv.config();
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(courseRoute.router);
 
 app.listen(process.env.PORT || 3001, () => {
-    console.log("Backend server is running! on port 8888")
+    console.log("Backend server is running! on port 3001")
 });
 
 export default app
